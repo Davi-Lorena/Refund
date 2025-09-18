390f5295eee11793c114c39c13eb2a3a04b8ea7e
Davi Lorena <davilorena2016@gmail.com>
2025-09-18 11:30:53 -0300

    feat: adiciona controller e rotas para refunds

---
+++ src/controllers/refunds-controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express"
+
+class RefundsController {
+async create(req: Request, res: Response) {
+
+    res.json({message: "ok"})
+}
+
+}
+
+export { RefundsController }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,9 +4,14 @@ import { userRoutes } from "./users-routes"
 
 import { sessionsRoutes } from "./sessions-routes"
 
+import { refundsRoutes } from "./refund-routes"
+
 const routes = Router()
 
 routes.use("/users", userRoutes)
 routes.use("/sessions", sessionsRoutes)
+
+// Private routes
+routes.use("/refunds", refundsRoutes)
 
 export { routes }
