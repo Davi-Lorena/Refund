c50102e9b19bec6c67cd58bb9441367ea9642527
Davi Lorena <davilorena2016@gmail.com>
2025-09-18 11:09:32 -0300

    feat: adiciona verificacao de email e senha para login

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,14 +1,35 @@
 import { Request, Response, NextFunction } from "express"
-import { object, z } from "zod"
+import { z } from "zod"
 import { prisma } from "@/database/prisma"
-import { userRole } from "@prisma/client"
 import { AppError } from "@/utils/AppError"
+import { compare } from "bcrypt"
 
 class SessionsController {
 
 async create(req: Request, res: Response) {
+const bodySchema = z.object({
+email: z.string().trim().email({ message: "E-mail inválido"}),
+password: z.string(), // Não validamos nada mais aqui, nem a quantidade mnínima de caracteres, porque faremos a comparação com a senha do banco
 
-    res.json({message:"ok"})
+})
+
+const { email, password } = bodySchema.parse(req.body)
+
+const user = await prisma.user.findFirst({ where: { email }})
+
+if(!user) {
+    throw new AppError("E-mail ou senha inválido!", 401)
+}
+
+const passwordMatched = await compare(password, user.password)
+
+if(!passwordMatched) {
+     throw new AppError("E-mail ou senha inválido!", 401)
+}
+
+
+
+    res.json({email, password})
 }
 
 }
--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express"
-import { object, z } from "zod"
+import { z } from "zod"
 import { prisma } from "@/database/prisma"
 import { userRole } from "@prisma/client"
 import { AppError } from "@/utils/AppError"
