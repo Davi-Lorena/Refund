08c4ae2b32ecf1956cf0adab26fdfe19a6af08d7
Davi Lorena <davilorena2016@gmail.com>
2025-09-18 10:34:55 -0300

    feat: adiciona validacao dos dados de usuario via zod

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response, NextFunction } from "express"
-import { z } from "zod"
+import { object, z } from "zod"
 import { prisma } from "@/database/prisma"
+import { userRole } from "@prisma/client"
 
 
 class UsersController {
 async create(req: Request, res: Response) {
 
-res.json({message: "ok"})
+const bodySchema = z.object({
+name: z.string().trim().min(2, { message: "Nome é obrigatório"}),
+email: z.string().trim().email({ message: "E-mail inválido"}).toLowerCase(),
+password: z.string().trim().min(6, { message: "A mensagem deve ter pelo menos 6 dígitos"}),
+role: z.enum([userRole.employee, userRole.manager]).default(userRole.employee),
+}) 
+
+const { name, email, password, role } = bodySchema.parse(req.body)
+
+
+res.json({ name, email, password, role })
 }
 
 }
