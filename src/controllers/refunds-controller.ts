0e040a8f6195321863f6ec2088dcd7954f0879f5
Davi Lorena <davilorena2016@gmail.com>
2025-10-16 11:36:40 -0300

    feat: valida dados de solicitacoes

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from "express"
+import { z } from "zod"
+
+const categoriesEnum = z.enum([
+    "food", 
+    "others", 
+    "services", 
+    "transport", 
+    "accomodation"
+])
 
 class RefundsController {
 async create(req: Request, res: Response) {
+const bodySchema = z.object({
+    name: z.string().trim().min(1,{ message: "Inform solicitation name"}), 
+category: categoriesEnum,
+amount: z.number().positive({message: "The value must be positive"}),
+filename: z.string().min(20)
+})
+
+const {name, category, amount, filename} = bodySchema.parse(req.body)
 
     res.json({message: "ok"})
 }
