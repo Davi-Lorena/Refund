fbeaa91235440857f1b5856c00d76dec30ce9e25
Davi Lorena <davilorena2016@gmail.com>
2025-10-16 11:43:28 -0300

    feat: altera a forma de validar a categoria, importando a enum do prisma client

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,19 +1,22 @@
 import { Request, Response } from "express"
 import { z } from "zod"
+import { Category } from "@prisma/client"
 
-const categoriesEnum = z.enum([
-    "food", 
-    "others", 
-    "services", 
-    "transport", 
-    "accomodation"
-])
+const categories = z.nativeEnum(Category)
+
+// const categoriesEnum = z.enum([
+//     "food", 
+//     "others", 
+//     "services", 
+//     "transport", 
+//     "accomodation"
+// ])
 
 class RefundsController {
 async create(req: Request, res: Response) {
 const bodySchema = z.object({
     name: z.string().trim().min(1,{ message: "Inform solicitation name"}), 
-category: categoriesEnum,
+category: categories,
 amount: z.number().positive({message: "The value must be positive"}),
 filename: z.string().min(20)
 })
