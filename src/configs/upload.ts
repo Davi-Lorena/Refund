e17d6df70ce9ac4f2823317f9227d4fd1a7a1bcc
Davi Lorena <davilorena2016@gmail.com>
2025-10-30 09:19:45 -0300

    feat: adiciona validacao de arquivos

--- src/configs/upload.ts
+++ src/configs/upload.ts
@@ -11,7 +11,8 @@ const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
 // Pasta de uploads pós-temporária
 const UPLOADS_FOLDER = path.relative(TMP_FOLDER, "uploads")
 
-const MAX_FILE_SIZE = 1024 * 1024 * 3 // 3MB (Obrigado, Minoru)
+const MAX_SIZE = 3
+const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE // 3MB (Obrigado, Minoru)
 
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
 
@@ -33,6 +34,7 @@ export default {
     TMP_FOLDER,
     UPLOADS_FOLDER,
     MULTER,
+    MAX_SIZE,
     MAX_FILE_SIZE,
     ACCEPTED_IMAGE_TYPES
 }
--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from "express"
+import z from "zod"
+
+import uploadConfig from "@/configs/upload"
 
 class UploadsController {
     async create(req: Request, res: Response) {
 
-        res.status(201).json({message: "ok"})
+try {
+    const fileSchema = z.object({
+        filename: z.string().min(1, "File is required"),
+        mimetype: z.string().refine((type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type), "File format invalid! Options: " + uploadConfig.ACCEPTED_IMAGE_TYPES),
+        size: z.number().positive().refine((size) => size <= uploadConfig.MAX_FILE_SIZE, `Maximum size exceeded (${uploadConfig.MAX_SIZE}mb)`)
+    }).passthrough()
+
+const { file } = fileSchema.parse(req.file)
+
+res.json({message: "OK!"})
+
+} catch (error) {
+    throw error
+}
+
     }
 }
 
